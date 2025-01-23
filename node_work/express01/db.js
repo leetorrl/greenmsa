99a96c61016393e7f9382bc4c5d7970476030509
DESKTOP-SCJ83VA\user <leetorrl123@gmail.com>
2025-01-23 13:42:05 +0900

    no message

---
+++ node_work/express01/db.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2');
+
+const select = ()=>{
+    const connection = mysql.createConnection({
+      host: '192.168.0.122',
+      user: 'root',
+      password :'1234',
+      database :'nodejs'
+    })
+    connection.connect(()=>{
+      console.log("db연결성공");
+    })
+    connection.query('select * from users',(err,results)=>{
+      console.log(results);
+      connection.end();
+    })
+    console.log("db연결종료");
+  }
+module.exports = {
+    select
+}
---
+++ node_work/express01/index.js
@@ -0,0 +1,37 @@
+
+
+const express = require('express')
+
+const db = require('./db');
+
+const app = express();
+app.use(express.json()); // json 파일 번역해서 req 에 달아주는 역활
+
+app.get('/',(req,res)=> {
+    res.send('hello');
+})
+
+app.get('/aa',(req,res)=> {
+    const name = "홍길동";
+    const age = 20;
+    res.send({name,age});
+})
+
+app.post('/save',(req,res)=> {
+
+    console.log("save 온다")
+    res.json({"status":200, message:"success"})
+
+})
+
+app.post("/login", (req, res) => {
+    db.select();
+    console.log(req.body);
+    res.json({ status: 200 });
+
+})
+
+
+app.listen(3000,()=>{
+console.log("서버시작했음")
+})
