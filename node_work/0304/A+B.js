865dc00f82b20b6fe4811fba373748b4b0299989
leetorrl <leetorrl123@gmail.com>
2025-03-05 17:01:22 +0900

    0305

--- node_work/0304/A+B.js
+++ node_work/0304/A+B.js
@@ -1,17 +1,19 @@
-// const fs = require('fs');
-// const [A, B] = fs.readFileSync(0, 'utf-8').trim().split(' ').map(Number);
-
-// console.log(A + B);
-
 const readline = require('readline');
 
+console.log("A + B + C = ??")
+
 const rl = readline.createInterface({
+  
   input: process.stdin,
   output: process.stdout
-});
+})
 
 rl.question("", (input) => {
-  const [A, B] = input.split(' ').map(Number);
-  console.log(A + B);
+
+  const [A, B, C] = input.split(' ').map(Number);
+  
+  console.log(A+B+C);
+
   rl.close();
-});
+
+})
